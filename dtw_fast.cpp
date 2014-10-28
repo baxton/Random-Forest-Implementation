adfdd571442b67ad82e8d39e10322845d0d06636
Maxim Alekseykin <max.uk2005@gmail.com>
2014-10-28 19:12:19 +0800

    v3

--- dtw_fast.cpp
+++ dtw_fast.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <limits>
 #include <cmath>
+#include <vector>
 
 //#include <emmintrin.h>
 
@@ -16,8 +17,8 @@
 using namespace std;
 
 
-#define X_LEN        10000
-#define DTW_VEC_LEN  (X_LEN + 1)
+//#define X_LEN        (600 - 59 - 59)
+//#define DTW_VEC_LEN  (X_LEN + 1)
 
 #define ALIGNMENT __attribute__((aligned(4)))
 
@@ -49,8 +50,8 @@ def DTWDistance(s1, s2, w):
     return sp.sqrt(DTW[len(s1), len(s2)])
 ****/
 
-float DTW [DTW_VEC_LEN * DTW_VEC_LEN] ALIGNMENT;
-
+//float DTW [DTW_VEC_LEN * DTW_VEC_LEN] ALIGNMENT;
+/*
 void fill(float v) {
 
     int size_1 = (DTW_VEC_LEN-1)*(DTW_VEC_LEN-1);
@@ -73,22 +74,32 @@ void fill(float v) {
         DTW[i] = v;
     }
 }
-
+*/
 
 extern "C" {
 
 __declspec(dllexport)
-int DTWDistance(const float* s1, const float* s2, int w, float giveup_threshold, float* ret) {
+int DTWDistance(const float* s1, const float* s2, int size1, int size2, int w, float giveup_threshold, float* ret) {
 
-    fill(numeric_limits<float>::infinity());
+    int DTW_ROWS = size1 + 1;
+    int DTW_VEC_LEN = size2 + 1;
+
+    //cout << "RET: " << *ret << endl;
+    //cout << "S1[0]: " << s1[0] << endl;
+
+    //cout << size1 << endl << size2 << endl << DTW_ROWS << endl << DTW_VEC_LEN << endl << w << endl;
+
+    vector<float> DTW(DTW_ROWS * DTW_VEC_LEN, numeric_limits<float>::infinity());
+    //cout << "DTW size: " << DTW.size() << endl;
 
     DTW[0] = 0.f;
 
+    float global_min_dist = numeric_limits<float>::infinity();
     bool giveup = false;
 
-    for (int i = 0; i < X_LEN && !giveup; ++i) {
+    for (int i = 0; i < size1 && !giveup; ++i) {
         int begin = (i - w) > 0 ? i - w : 0;
-        int end = (i + w) < X_LEN ? i + w : X_LEN;
+        int end = (i + w) < size2 ? i + w : size2;
 
         float min_dist = numeric_limits<float>::infinity();
 
@@ -106,24 +117,27 @@ int DTWDistance(const float* s1, const float* s2, int w, float giveup_threshold,
 
             if (min_dist > DTW[dtwI * DTW_VEC_LEN + dtwJ]) {
                 min_dist = DTW[dtwI * DTW_VEC_LEN + dtwJ];
-
-                if (min_dist > giveup_threshold) {
-                    // in this case infinity will be returned
-                    giveup = true;
-                    break;
-                }
+                global_min_dist = min_dist;
             }
+        }
+
+        if (min_dist > giveup_threshold) {
+            giveup = true;
+            //cout << "giveup" << endl;
         }
     }
 
-    *ret = sqrt( DTW[DTW_VEC_LEN*DTW_VEC_LEN - 1] );
+    *ret = giveup ? global_min_dist : DTW[(DTW_ROWS-1) * DTW_VEC_LEN + (DTW_VEC_LEN - 1)];
 
-    return !giveup ? 1 : 0;
+    return !giveup;
 }
 
 
 
+
+
 }
+
 
 
 

f7776c711bb7255ec0bb4fbbc9092384dc0bf538
Maxim Alekseykin <max.uk2005@gmail.com>
2014-10-28 19:12:59 +0800

    Update features.py

--- features.py
+++ features.py
@@ -5,11 +5,36 @@ import scipy as sp
 import numpy.fft as fft
 
 
-X_LEN = 10000
 
-READ_LEN = 1 + 2 + X_LEN
+X_LEN = 600
+MA = X_LEN / 10
+
+READ_LEN = 1 + 1 + 1 + (X_LEN - MA + 1 - MA + 1)
 
 
+
+
+def moving_average(a, n=3) :
+    if 1 == len(a.shape):
+        ret = np.cumsum(a, dtype=float)
+        ret[n:] = ret[n:] - ret[:-n]
+        return ret[n - 1:] / n
+    else:
+        ret = np.cumsum(a, dtype=float, axis=1)
+        ret[:,n:] = ret[:,n:] - ret[:,:-n]
+        return ret[:,n - 1:] / n
+
+
+
+def Z_normalize(vec):
+    m = vec.mean()
+    #s = vec.std()
+    s = max( vec.max(), np.abs(vec.min()) )
+
+    vec -= m
+    vec /= s
+
+    return vec
 
 
 
@@ -20,123 +45,29 @@ def PAA(ORIG, bins):
 
     vec = sp.zeros((bags,), dtype=np.float32)
 
-    for i in range(bags):
+    for i in range(bags-1):
         begin = i * bag_size
-        end = begin + bag_size if begin + bag_size < N else N
-        if (end + end/2) >= N:
-            end = N
+        end = begin + bag_size
         vec[i] = ORIG[begin:end].mean()
 
-    return vec
-
-
-def PAA_ip(ORIG, bins):
-    '''
-    in place
-    '''
-    N = ORIG.shape[0]
-    bags = bins
-    bag_size = N / bags
-
-    for i in range(bags):
-        begin = i * bag_size
-        end = begin + bag_size if begin + bag_size < N else N
-        if (end + bag_size/2) >= N:
-            end = N
-        ORIG[i] = ORIG[begin:end].mean()
-
-    return ORIG
-
-
-
-
-def PA_max(ORIG, bins):
-    N = ORIG.shape[0]
-    bags = bins
-    bag_size = N / bags
-
-    vec = sp.zeros((bags,), dtype=np.float32)
-
-    for i in range(bags):
-        begin = i * bag_size
-        end = begin + bag_size if begin + bag_size < N else N
-        if (end + bag_size/2) >= N:
-            end = N
-        vec[i] = ORIG[begin:end].max()
+    vec[-1] = ORIG[end:].mean()
 
     return vec
 
-
-def PA_min(ORIG, bins):
-    N = ORIG.shape[0]
-    bags = bins
-    bag_size = N / bags
-
-    vec = sp.zeros((bags,), dtype=np.float32)
-
-    for i in range(bags):
-        begin = i * bag_size
-        end = begin + bag_size if begin + bag_size < N else N
-        if (end + bag_size/2) >= N:
-            end = N
-        vec[i] = ORIG[begin:end].min()
-
-    return vec
-
-
-
-
-def PA_integral(ORIG, bins):
-    N = ORIG.shape[0]
-    bags = bins
-    bag_size = N / bags
-
-    vec = sp.zeros((bags,), dtype=np.float32)
-
-    for i in range(bags):
-        begin = i * bag_size
-        end = begin + bag_size if begin + bag_size < N else N
-        if (end + bag_size/2) >= N:
-            end = N
-        vec[i] = ORIG[begin:end].sum()
-
-    return vec
-
-
-def DV(ORIG, bins):
-    tmp = ORIG[1:] - ORIG[0:-1]
-    return PAA_ip(tmp, bins)
 
 
 
 
 
 def extract_features(ts):
-    #ts -= ts.mean()
 
-##    ts = ts[1:] - ts[0:-1]
-##    tmp = tmp[0:-1] - tmp[1:]
-##
-    features1 = PAA_ip(ts, X_LEN)
-    ret = features1[:X_LEN].astype(np.float32)
-##
-##    features2 = fft.fft(ts, X_LEN).astype(np.float64)
-##    features3 = fft.fftfreq(X_LEN)
+    ts = ts.astype(np.float32)
 
-    #features4 = PAA(ts, X_LEN)
+    features1 = Z_normalize(ts)
+    features1 = PAA(features1, X_LEN)
+    features1 = moving_average(features1, MA)
+    features1 = moving_average(features1, MA)
+    ret = features1.astype(np.float32)
 
-##    features5 = PA_max(ts, X_LEN)
-
-
-    #return sp.concatenate((features1, features2, features3))
-
-    #ret = sp.concatenate((features1, features2, features3, features4, features5))
-    #ret = np.array([features4.mean(), features4.max(), features4.min(), features4.std()], dtype=np.float64)
-
-##    if READ_LEN != (ret.shape[0] + 1 + 2):
-##        raise Exception("READ_LEN is not adjusted %d vs %d" % (READ_LEN, ret.shape[0]+1))
-
-##    if X_LEN != ret.shape[0]:
-##        raise Exception("X_LEN is not adjusted %d vs %d" % (X_LEN, ret.shape[0]))
 
     return ret

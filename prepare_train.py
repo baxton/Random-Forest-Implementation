548680445c1acd563a4c17a625dad1f061b528f3
Maxim Alekseykin <max.uk2005@gmail.com>
2014-11-10 18:43:14 +0800

    v4

--- prepare_train.py
+++ prepare_train.py
@@ -14,11 +14,11 @@ import files
 import features
 
 
-DLL = ctypes.cdll.LoadLibrary("C:\\Temp\\kaggle\\epilepsy\\scripts\\sub5\\dtw_fast.dll")
+#DLL = ctypes.cdll.LoadLibrary("C:\\Temp\\kaggle\\epilepsy\\scripts\\sub6\\dtw_fast.dll")
 
 
 path = "C:\\Temp\\kaggle\\epilepsy\\data\\"
-path_train = path + "prepared_sub5\\"
+path_train = path + "prepared_sub6\\"
 
 
 interictal_prefix = "interictal_segment"
@@ -30,7 +30,6 @@ INTERICTAL_CLS = 0
 
 X_LEN = features.X_LEN
 
-W=50
 
 
 
@@ -53,9 +52,26 @@ def get_subj(f_id):
     return DOG
 
 
+def get_segment(fname):
+    if "interictal" in fname:
+        return "interictal_segment"
+    elif "preictal" in fname:
+        return "preictal_segment"
+
+    return"test_segment"
+
+
+def get_cls(segment):
+    if "interictal" in segment:
+        return INTERICTAL_CLS
+
+    return PREICTAL_CLS
+
+
+
 def DTWDistance(s1, s2, w, giveup_threshold = float('inf')):
     res = ctypes.c_float(0.)
-    code = DLL.DTWDistance(s1.ctypes.data, s2.ctypes.data, ctypes.c_int(w), ctypes.c_float(giveup_threshold), ctypes.addressof(res))
+    #code = DLL.DTWDistance(s1.ctypes.data, s2.ctypes.data, ctypes.c_int(w), ctypes.c_float(giveup_threshold), ctypes.addressof(res))
     return res.value, code
 
 
@@ -106,64 +122,137 @@ def get_data_matrix(f, key_word):
 
 
 
+def get_cor(v1, v2):
+    L = min(v1.shape[0], v2.shape[0])
+    cr = np.corrcoef(v1[:L], v2[:L])
+    return cr[0,1]
+
+
+
+
+def to_features(orig):
+    trend, seson, noise = features.extract_features( orig )
+
+    cum_sum = np.cumsum(seson)
+    cum_sum /= (cum_sum.max() - cum_sum.min())
+    seson_norm = seson - cum_sum
+
+    # stats
+    vec = sp.concatenate((
+        [
+            orig.std(),
+            orig.min(),
+            orig.max(),
+            np.median(orig),
+
+            trend.std(),
+            trend.min(),
+            trend.max(),
+            np.median(trend),
+
+            seson.std(),
+            seson.min(),
+            seson.max(),
+            np.median(seson),
+
+            noise.std(),
+            noise.min(),
+            noise.max(),
+            np.median(noise),
+
+            cum_sum.std(),
+            cum_sum.min(),
+            cum_sum.max(),
+            np.median(cum_sum),
+
+            seson_norm.std(),
+            seson_norm.min(),
+            seson_norm.max(),
+            np.median(seson_norm),
+
+            get_cor(orig, trend),
+            get_cor(orig, seson),
+            get_cor(orig, noise),
+            get_cor(orig, cum_sum),
+            get_cor(orig, seson_norm),
+
+            get_cor(trend, seson),
+            get_cor(trend, noise),
+            get_cor(trend, cum_sum),
+            get_cor(trend, seson_norm),
+
+            get_cor(seson, noise),
+            get_cor(seson, cum_sum),
+            get_cor(seson, seson_norm),
+
+            get_cor(noise, cum_sum),
+            get_cor(noise, seson_norm),
+
+            get_cor(cum_sum, seson_norm),
+        ],
+        ))
+    return vec
+
+
+
+def to_features2(orig):
+    trend, seson, noise = features.extract_features( orig )
+
+    v1 = fft.fft(trend).astype(np.float32)
+    v2 = fft.fft(seson).astype(np.float32)
+    v3 = fft.fft(noise).astype(np.float32)
+
+    return sp.concatenate((v1[:10], v2[:10], v3[:10]))
 
 
 def process():
 
-    log = open(path + "..\\logs\\prepare_train.sub5.log", "w+")
+    log = open(path + "..\\logs\\prepare_train.sub6.log", "w+")
+
+    fout_prefix = "fft"
 
 
     #########################################################
     ## select train files
     #########################################################
 
-    #train_ii_indices = [72,116,203,250,265,328,341,355,436,458,486,526,593,612,613,672,705,795,904,941,1070,1160,1284,1287,1447,1555,1652,2009,2330,2383,2447,2590,2725,2857,2906,2955,2960,3020,3161,3171,3311,3409,3546,3558,3563,3576,3589,3645,3653,3655,3675,3686,3688,3694,3695,3696,3698,3702,3710,3719,3727,3730,3734,3735,3738,3740,3743,3746,3755,3764]
-    #train_pi_indices = [3768,3769,3771,3776,3777,3779,3780,3781,3787,3788,3791,3795,3802,3805,3806,3808,3812,3816,3826,3831,3835,3842,3849,3852,3853,3854,3864,3873,3885,3890,3909,3918,3924,3933,3948,3955,3965,3973,3987,3989,4005,4006,4013,4014,4017,4018,4022,4023,4025,4027,4032,4034,4035,4036,4038,4040,4041,4043,4046,4048,4051,4052,4053,4054,4057,4058,4059,4062,4064,4065]
-    train_ii_indices = [33,44,51,100,145,165,191,194,200,224,251,265,322,326,344,346,366,373,420,441,519,535,545,583,602,605,626,627,630,651,657,700,770,775,776,786,848,896,905,950,999,1038,1134,1137,1195,1539,1554,1606,1676,1691,1779,1861,1880,1891,1968,2002,2109,2174,2321,2347,2475,2484,2528,2581,2591,2764,2781,2804,2855,2903,2979,2983,3027,3036,3038,3072,3139,3144,3161,3192,3237,3241,3245,3269,3270,3293,3305,3318,3368,3393,3411,3431,3452,3453,3482,3531,3554,3605,3647,3667,3677,3678,3681,3685,3686,3690,3697,3699,3700,3701,3703,3704,3705,3707,3708,3710,3714,3715,3718,3720,3724,3728,3729,3730,3732,3733,3735,3736,3738,3744,3748,3749,3752,3754,3756,3757,3758,3760,3761,3762]
-    train_pi_indices = [3768,3770,3771,3772,3776,3778,3779,3781,3786,3789,3795,3800,3801,3805,3806,3809,3815,3823,3824,3829,3841,3848,3855,3864,3869,3871,3880,3885,3894,3903,3905,3915,3917,3926,3928,3930,3972,3977,3995,4000,4001,4005,4012,4018,4021,4022,4023,4024,4027,4028,4031,4033,4035,4036,4037,4041,4042,4043,4046,4047,4049,4050,4052,4053,4055,4056,4062,4063,4065,4066]
+    train_ii_indices = [6,65,194,271,275,343,412,444,451,469,498,547,592,661,663,825,851,866,900,907,1158,1232,1438,1464,1686,1746,1834,2049,2099,2250,2475,2509,2588,2618,2628,2792,2897,2903,3042,3175,3315,3383,3421,3437,3458,3544,3599,3645,3652,3655,3674,3688,3689,3693,3695,3702,3705,3712,3716,3720,3730,3735,3736,3744,3748,3749,3751,3756,3758,3762]
+    train_pi_indices = [3767,3768,3769,3771,3772,3779,3780,3782,3785,3789,3796,3797,3800,3804,3809,3811,3818,3822,3828,3829,3851,3852,3854,3872,3874,3877,3884,3886,3890,3899,3906,3916,3930,3938,3942,3946,3957,3984,3997,3998,4004,4010,4012,4013,4019,4020,4025,4027,4028,4030,4031,4032,4036,4039,4040,4042,4044,4045,4047,4048,4049,4050,4051,4052,4054,4055,4060,4061,4064,4065]
+
+    test_ii_indices = [323,415,296,223,108,154,206,43,456,134,841,816,849,542,505,533,703,487,511,795,1061,1921,1704,1385,1504,1253,1286,2073,1505,1466,2781,2870,2866,2835,2913,2624,2671,2807,3082,3074,3647,3640,3427,3608,3525,3442,3620,3459,3504,3665,3675,3676,3719,3678,3679,3699,3709,3708,3710,3697,3764,3747,3752,3727,3750,3757,3737,3743,3733,3734]
+    test_pi_indices = [3766,3770,3773,3774,3775,3784,3777,3778,3781,3787,3790,3806,3803,3826,3805,3831,3821,3799,3820,3812,3902,3857,3863,3864,3882,3893,3869,3850,3840,3861,3924,3987,3927,3908,3981,3922,3975,3962,3917,3945,4001,4002,4016,4023,4021,4007,4017,4024,4011,4015,4033,4034,4035,4037,4038,4041,4043,4046,4053,4056,4057,4058,4059,4062,4063,4066]
+
     #########################################################
 
     fn_cnt = 0
-    fname_train = "%strain_%d.csv" % (path_train, fn_cnt)
+    fn_out = "%s%s_%d.csv" % (path_train, fout_prefix, fn_cnt)
+    fout = open(fn_out, "wb+")
     fn_cnt += 1
 
     cnt = 0
 
-    #with open(fname_train, "wb+") as fout:
-    if True:
-        fout = open(fname_train, "wb+")
-
-        for i in train_ii_indices:
-            cnt += 1
+    for i in (train_ii_indices + train_pi_indices):
+        cnt += 1
+        if i in files.INTERICTAL_FILES:
             fn = files.INTERICTAL_FILES[ i ]
-            data_matrix, freq, sensors, length = get_data_matrix(fn, 'interictal')
-            for s in range(sensors):
-                vec = features.extract_features( data_matrix[s] )
-                write(fout, INTERICTAL_CLS, vec, i, s)
-
-            if (cnt % 20) == 0:
-                dbg(log, "currently processed %d files" % cnt)
-
-                fname_train = "%strain_%d.csv" % (path_train, fn_cnt)
-                fn_cnt += 1
-                fout.close()
-                fout = open(fname_train, "wb+")
-
-        for i in train_pi_indices:
-            cnt += 1
+        else:
             fn = files.PREICTAL_FILES[ i ]
-            data_matrix, freq, sensors, length = get_data_matrix(fn, 'preictal')
-            for s in range(sensors):
-                vec = features.extract_features( data_matrix[s] )
-                write(fout, PREICTAL_CLS, vec, i, s)
 
-            if (cnt % 20) == 0:
-                dbg(log, "currently processed %d files" % cnt)
+        seg = get_segment(fn)
+        cls = get_cls(seg)
 
-                fname_train = "%strain_%d.csv" % (path_train, fn_cnt)
-                fn_cnt += 1
-                fout.close()
-                fout = open(fname_train, "wb+")
+        data_matrix, freq, sensors, length = get_data_matrix(fn, seg)
+
+        for sen in range(sensors):
+            vec = to_features2(data_matrix[sen])
+            write(fout, cls, vec, i, sen)
+
+        if (cnt % 20) == 0:
+            dbg(log, "currently processed %d files" % cnt)
+            fn_out = "%s%s_%d.csv" % (path_train, fout_prefix, fn_cnt)
+            fn_cnt += 1
+            fout.close()
+            fout = open(fn_out, "wb+")
 
     dbg(log, "DONE, %d files" % cnt)
 

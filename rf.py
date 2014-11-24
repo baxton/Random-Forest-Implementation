b07c7916e2fc2886c9b958b927ea21e1f2ce8dc8
Maxim Alekseykin <max.uk2005@gmail.com>
2014-11-24 18:03:17 +0800

    v7

--- rf.py
+++ rf.py
@@ -1,65 +1,108 @@
+############################################################
+#
+#
+#
+############################################################
 
 
+import os
+import sys
 import numpy as np
 import scipy as sp
 import ctypes
 
+import pathes
+import utils
 
-RF_DLL = ctypes.cdll.LoadLibrary("C:\\Temp\\kaggle\\epilepsy\\scripts\\sub5\\rf.dll")
+
+
+
+RF_DLL = ctypes.cdll.LoadLibrary(pathes.path_base + "scripts/sub1/rf.dll")
+
+
 
 
 class RF(object):
-    def __init__(self, trees, k, lnum):
-        self.trees = trees
-        self.k = k
-        self.lnum = lnum
+
+    def __init__(self, columns, trees_num=10, nfeatures=5, samples_per_leaf=5):
+        self.trees_num = trees_num
+        self.nfeatures = nfeatures
+        self.samples_per_leaf = samples_per_leaf
+
+        self.trees = []
+
+        for t in range(self.trees_num):
+            ptree = RF_DLL.alloc_tree_learner(ctypes.c_int(columns), ctypes.c_int(self.nfeatures), ctypes.c_int(self.samples_per_leaf))
+            self.trees.append(ptree)
+    # END of ctor
+
+    def tofile(self, fname):
+        for t in range(self.trees_num):
+            fn = fname + str(t) + ".rf"
+            cfn = ctypes.c_char_p(fn)
+            RF_DLL.tofile_tree(ctypes.c_void_p(self.trees[t]), cfn)
+
+    def release(self):
+        for t in range(self.trees_num):
+            RF_DLL.free_tree(ctypes.c_void_p(self.trees[t]))
+
+        self.trees = []
+    # END of release
+
+    def prepare_bootstraps(self, num_in_bootstrap, total_num):
+        self.bootstraps = []
+        for t in range(self.trees_num):
+            self.bootstraps.append( {} )
+            for i in sp.random.randint(0, total_num, num_in_bootstrap):
+                if i in self.bootstraps[t]:
+                    self.bootstraps[t][i] += 1
+                else:
+                    self.bootstraps[t][i] = 1
+    # END of prepare_bootstraps
+
+    def start_fit(self):
+        for t in range(self.trees_num):
+            RF_DLL.start_fit_tree(ctypes.c_void_p(self.trees[t]))
+
+
+    def fit(self, id, x, y):
+        for t in range(self.trees_num):
+            if id in self.bootstraps[t]:
+                for i in range(self.bootstraps[t][id]):
+                    RF_DLL.fit_tree(ctypes.c_void_p(self.trees[t]), x.ctypes.data, ctypes.c_double(y))
 
 
 
-    def alloc(self):
-        RF_DLL.alloc_rf(ctypes.c_int(self.trees), ctypes.c_int(self.k), ctypes.c_int(self.lnum))
-
-    def dealloc(self):
-        RF_DLL.dealloc_rf()
-
-    def fit(self, X, Y):
-        X = np.array(X, dtype=float)
-        Y = np.array(Y, dtype=float)
-
-        rows = X.shape[0]
-        columns = X.shape[1]
-        RF_DLL.fit_rf(X.ctypes.data, Y.ctypes.data, ctypes.c_int(rows), ctypes.c_int(columns))
-
-    def predict(self, X):
-        X = np.array(X, dtype=float)
-
-        res = []
-
-        if len(X.shape) == 2:
-            rows = X.shape[0]
-            columns = X.shape[1]
-
-            for r in range(rows):
-                v = X[r, :]
-                p = ctypes.c_double(0.)
-                RF_DLL.predict_rf(v.ctypes.data, ctypes.c_int(columns), ctypes.addressof(p))
-
-                p = p.value
-                res.append( [1. - p, p] )
-        else:
-            columns = X.shape[0]
-            v = X
-            p = ctypes.c_double(0.)
-            RF_DLL.predict_rf(v.ctypes.data, ctypes.c_int(columns), ctypes.addressof(p))
-
-            p = p.value
-            res.append( [1. - p, p] )
+    def stop_fit(self):
+        for t in range(self.trees_num):
+            RF_DLL.stop_fit_tree(ctypes.c_void_p(self.trees[t]))
 
 
-        return np.array(res, dtype=float)
+
+    def is_finished(self):
+        cnt = 0
+        for t in range(self.trees_num):
+            cnt += RF_DLL.end_of_splitting(ctypes.c_void_p(self.trees[t]))
+
+        return cnt == self.trees_num
 
 
-    def predict_proba(self, X):
-        return self.predict(X)
-# END OF RF
+    def predict(self, x):
+        p = 0.
+        for t in range(self.trees_num):
+            tmp_p = ctypes.c_double(0)
+            RF_DLL.predict(ctypes.c_void_p(self.trees[t]), x.ctypes.data, ctypes.addressof(tmp_p))
+            p += tmp_p.value
+        return p / self.trees_num
+
+
+    def predict_proba(self, x):
+        p = self.predict(x)
+        return np.array([ 1. - p, p ])
+
+
+
+    def print_rf(self):
+        RF_DLL.print_tree(ctypes.c_void_p(self.trees[0]))
+
 

c8d203db8dab38a1b16f0b48699f821a8905cf67
Maxim Alekseykin <max.uk2005@gmail.com>
2015-01-27 17:19:28 +0800

    v1

---
+++ make_train.py
@@ -0,0 +1,76 @@
+
+
+import os
+from array import array
+import pathes
+
+
+VEC_LEN = 10
+VECTORS_PER_FILE = 3000000
+
+
+fname = pathes.path_train + "train.csv.b"
+
+fin = open(fname, "rb")
+files = [
+    open(pathes.path_lr_data + "ent_90_6_51_1.b_train.txt", "r"),
+    open(pathes.path_lr_data + "ent_90_6_51_0(1).b_train.txt", "r"),
+    open(pathes.path_lr_data + "ent_90_6_51_2.b_train.txt", "r"),
+]
+
+fname_out_pref = pathes.path_lr_data + "lr_train.b"
+
+def main():
+    vectors_total = 0
+
+    cnt = 0
+    fname_out = "%s.%d" % (fname_out_pref, cnt)
+    fout = open(fname_out, "wb+")
+
+    vec = array('f', [0.] * VEC_LEN)
+
+    vectors_written = 0
+
+    try:
+        while True:
+            a = array('l')
+            a.fromfile(fin, 32)
+
+            idx = 0
+            vec[idx] = a[1]   # click
+            idx += 1
+
+            for f in files:
+                line = f.readline().strip()
+                while line.startswith("#"):
+                    line = f.readline().strip()
+
+                val = float(line)
+                vec[idx] = val
+                idx += 1
+                vec[idx] = val ** 2
+                idx += 1
+                vec[idx] = val ** 3
+                idx += 1
+
+            vec.tofile(fout)
+            vectors_total += 1
+            vectors_written += 1
+
+            if vectors_written >= VECTORS_PER_FILE:
+                vectors_written = 0
+                fout.close()
+                cnt += 1
+                fname_out = "%s.%d" % (fname_out_pref, cnt)
+                fout = open(fname_out, "wb+")
+
+
+    except Exception as ex:
+        print ex, "EOF, processed", vectors_total, "vectors"
+
+    fout.close()
+
+
+
+if __name__ == '__main__':
+    main()
